05e07a02bcaa5d625124c428022bda0a61b588c0
Misty Blue <hellomistyblue@gmail.com>
2025-11-18 11:51:01 -0600

    initial setup

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -6,5 +6,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register'),
 ]
 
